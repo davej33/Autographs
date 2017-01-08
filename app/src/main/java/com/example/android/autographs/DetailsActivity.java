6594e978f1f83991216b270fa721a13290f8b058
David Justen <davejusten03@yahoo.com>
2017-01-08 13:03:25 -0500

    periodic save. can load image. working on saving and updating

--- app/src/main/java/com/example/android/autographs/DetailsActivity.java
+++ app/src/main/java/com/example/android/autographs/DetailsActivity.java
@@ -8,8 +8,10 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.percent.PercentRelativeLayout;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AlertDialog;
@@ -22,12 +24,15 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.android.autographs.data.InventoryContract;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -45,12 +50,15 @@ public class DetailsActivity extends AppCompatActivity implements LoaderManager.
     private EditText mQuantInsert;
     private EditText mSupInsert;
     private EditText mSupEmailInsert;
+    private ImageView mImageInsert;
     private TextView mItemId;
     private String mTransactionTime;
     private String mSupplier;
     private String mSupplierEmail;
     private int mInStock;
+    private byte[] mImage;
     private static final int SALE = 1;
+    private static int RESULT_LOAD_IMAGE = 1;
 
 
     DialogInterface.OnClickListener dialogueDismiss = new DialogInterface.OnClickListener() {
@@ -73,10 +81,6 @@ public class DetailsActivity extends AppCompatActivity implements LoaderManager.
         }
     };
 
-    public static String formatCurrency(Double d) {
-        DecimalFormat df = new DecimalFormat("0.00");
-        return df.format(d);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -114,6 +118,8 @@ public class DetailsActivity extends AppCompatActivity implements LoaderManager.
         mSupInsert = (EditText) findViewById(R.id.insert_supplier);
         mSupEmailInsert = (EditText) findViewById(R.id.insert_supplier_email);
         mItemId = (TextView) findViewById(R.id.product_id);
+        mImageInsert = (ImageView) findViewById(R.id.image_x);
+
 
         // set onTouch listeners
         mNameInsert.setOnTouchListener(mOnTouch);
@@ -121,6 +127,16 @@ public class DetailsActivity extends AppCompatActivity implements LoaderManager.
         mQuantInsert.setOnTouchListener(mOnTouch);
         mSupInsert.setOnTouchListener(mOnTouch);
         mSupEmailInsert.setOnTouchListener(mOnTouch);
+        mImageInsert.setOnTouchListener(mOnTouch);
+
+        mImageInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
+
 
         Button plusButton = (Button) findViewById(R.id.quantity_plus);
         plusButton.setOnClickListener(new View.OnClickListener() {
@@ -394,6 +410,7 @@ public class DetailsActivity extends AppCompatActivity implements LoaderManager.
         }
         insertValues.put(InventoryContract.Inventory.ITEM_SALE_PRICE, priceSet);
 
+
         int quantSet = 0;
         if (!quantity.isEmpty()) {
             quantSet = Integer.parseInt(quantity);
@@ -559,6 +576,39 @@ public class DetailsActivity extends AppCompatActivity implements LoaderManager.
         return loader;
 
 
+    }
+
+    public static String formatCurrency(Double d) {
+        DecimalFormat df = new DecimalFormat("0.00");
+        return df.format(d);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+            mImageInsert.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
+            try {
+                FileInputStream fis = new FileInputStream(picturePath);
+                mImage = new byte[fis.available()];
+                fis.read(mImage);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+           //  BitmapFactory.decodeFile(picturePath);
+
+            Log.e(LOG_TAG, "picture path string: " + picturePath);
+        }
     }
 
     @Override
